7c55bc2bb6051aff89509ba691453bbe38d4ef83
Agnes Jang <agnesj@uci.edu>
2020-08-16 01:02:23 -0700

    actually fix it this time

--- backend/server.py
+++ backend/server.py
@@ -7,14 +7,13 @@ from PySide2 import QtWidgets
 import syntax
 
 app = Flask(__name__)
+qapp = QtWidgets.QApplication(sys.argv)
 
 
 @app.route("/process_text", methods=["POST"])
 def process_text():
     data = request.json
-    app = QtWidgets.QApplication(sys.argv)
     editor = QtWidgets.QPlainTextEdit()
     highlighter = syntax.PythonHighlighter(editor.document())
     processed_code = highlighter.highlightBlock(data["data"])
-    del app
     return {"data": processed_code}
